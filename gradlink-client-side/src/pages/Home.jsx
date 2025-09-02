7960364e0c1efb3d399f6d24c942bf3c8287906f
hey-its-Sohan <its.sohan.dev@gmail.com>
2025-09-02 20:41:44 +0600

    created sections for homepage

--- gradlink-client-side/src/pages/Home.jsx
+++ gradlink-client-side/src/pages/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Hero from '../Components/Hero';
 import FeaturesSection from '../Components/FeaturesSection';
+import Testimonials from '../Components/Testimonials';
+import CTA from '../Components/CTA';
+import Impacts from '../Components/Impacts';
+import FAQ from '../Components/FAQ';
+import Partners from '../Components/Partners';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <FeaturesSection />
+      <Testimonials />
+      <CTA />
+      <Impacts />
+      <FAQ />
+      <Partners />
     </div>
   );
 };
